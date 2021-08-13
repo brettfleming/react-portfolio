a221f661eedd201960f8c5f4f83ab58b783bef9f
Brett Fleming <brettfleming015@gmail.com>
2021-08-13 13:38:15 -0500

    fixed the last things hopefully

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,8 +8,8 @@ function Contact() {
             <div>
                 <p className='contact-me-p'>Reach out to me which whichever method you prefer below!</p>
                     <ul className='contact-me-list d-flex justify-content-around'>
-                        <li><a href="mailto:brett2133424235324@gmail.com" target='_blank'>email</a></li>
-                        <li>555.555.5555</li>
+                        <li><a href="mailto:brettfleming015@gmail.com" target='_blank'>brettfleming015@gmail.com</a></li>
+                        <li>651.764.9441</li>
                         <li><a href="https://linkedin.com/in/brettfleming15" target='_blank'>linkedin</a></li>
                         <li><a href="https://github.com/brettfleming" target='_blank'>GitHub</a></li>
                     </ul>
