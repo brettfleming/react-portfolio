e1614c978bf2898d8e9f47df2d664927f92e8071
Brett Fleming <brettfleming015@gmail.com>
2021-08-13 08:50:27 -0500

    finsihed up the last remaining details

--- src/pages/About.js
+++ src/pages/About.js
@@ -25,13 +25,7 @@ function About() {
                 <div className='d-flex'>
                     <img className='img-me col-4 p-5'src={Me}/>
                     <p className='col-8 p-2'>
-                        I graduted from high school in 2016, I ended up attending Luther college in the small town of Decorah Iowa in pursuit of a chemistry degree.
-                        I attended Luther for four years playing soccer for all of them, I played in the attack as a winger and as a striker. In my four years on the varsity soccer team we won 
-                        four trophies (The league once and the confrence tournament three times), while also qualifying for the national tournment three years out of four. placeing in the top 8 two years in
-                        a row. Even though I was a student athlete that didnt mean I slacked on my studies, I was a student first, athlete secound ( hence <strong>student</strong> athlete). when I 
-                        wasnt practicing soccer I was at the library working on all my homework and studying for exams. I had to work very hard to keep up with my studies while also being an athlete 
-                        but i accepted the challenge head on! After graduating college in 2020 during a global pandemic it was a struggle to find a job, so almost a year later I decided to take this coding bootcamp to either make my self
-                        more marketable or change careers entirly. and I have been loving this bootcamp so far and I cant wait to add more to this portfolio as time goes on!
+                    I'm a Luther College graduate, I graduated in May of 2020 with a chemistry degree. In my four years of college I played soccer for the varsity soccer team and walked away with four trophies to my name while also  participating in the national tournament three of my four years at Luther. With back to back elite 8 finishes and a final ranking of 5th in the nation for my senior year. I also recently received a certificate in web-development after finishing an intensive 12-week coding Bootcamp at the University of Minnesota. During the bootcamp I really fell in love with web development and computer programming and I'm ready to use the skills that I have learned to create some amazing projects in the future!
                     </p>
                 </div>
             </div>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,6 +4,11 @@ import Cccbeer from '../assets/photos/landings_page_screenshot.png'
 import Bootstrap from '../assets/photos/skills-images/bootstrap.png';
 import Html5 from '../assets/photos/skills-images/html5.png';
 import Css3 from '../assets/photos/skills-images/css3.png';
+import Node from '../assets/photos/skills-images/nodeJS.png';
+import Express from '../assets/photos/skills-images/express.png';
+import Mysql from '../assets/photos/skills-images/MySQL.jpg';
+import Js from '../assets/photos/skills-images/JS.png';
+
 
 function Projects() {
     return (
@@ -16,14 +21,16 @@ function Projects() {
                         <div className="project-body">
                             <h3 className='project-title'>D&D Character Creator</h3>
                             <p className='poject-desc '>
-                                I graduted from high school in 2016, I ended up attending Luther college in the small town of Decorah Iowa in pursuit of a chemistry degree.
-                                I attended Luther for four years playing soccer for all of them, I played in the attack as a winger and as a striker. In my four years on the varsity soccer team we won 
-                                four trophies (The league once and the confrence tournament three times), while also qualifying for the national tournment three years out of four. placeing in the top 8 two years in
+                            An interactive webpage that is a D&D character creator, that allows users to look at other peoples characters and comment on them. They can also look up monsters in the monster index. 
                             </p>
                             <h3 className='project-tech'>Tech Stack</h3>
                                 <img className="skills-image" src={Html5} alt="HTML5"/>
                                 <img className="skills-image" src={Css3} alt="CSS3"/>
                                 <img className="skills-image" src={Bootstrap} alt="Bootstrap"/>
+                                <img className="skills-image" src={Js} alt="JavaScript" />
+                                <img className="skills-image" src={Node} alt="NodeJS" />
+                                <img className="skills-image" src={Express} alt="ExpressJS" />
+                                <img className="skills-image" src={Mysql} alt="MySQL" />
                             <div className='project-btn'>
                                 <button type="button" class="btn btn-primary project-btn"><a href="https://github.com/brettfleming/DnD-creator" target="_blank">GitHub</a></button>
                                 <button type="button" class="btn btn-success project-btn"><a href="https://dnd-creator.herokuapp.com/" target="_blank">Demo</a></button>
@@ -35,14 +42,13 @@ function Projects() {
                         <div className="project-body">
                             <h3 className='project-title'>Cocktails, Cuisine & Craft Beers</h3>
                             <p className='poject-desc '>
-                                I graduted from high school in 2016, I ended up attending Luther college in the small town of Decorah Iowa in pursuit of a chemistry degree.
-                                I attended Luther for four years playing soccer for all of them, I played in the attack as a winger and as a striker. In my four years on the varsity soccer team we won 
-                                four trophies (The league once and the confrence tournament three times), while also qualifying for the national tournment three years out of four. placeing in the top 8 two years in
-                            </p>
+                            A website that searches 3 different API's depending on what the user is looking for. The options are restaurants and breweries by Zip code or Cocktails by type or main ingredient                            </p>
                             <h3 className='project-tech'>Tech Stack</h3>
                                 <img className="skills-image" src={Html5} alt="HTML5"/>
                                 <img className="skills-image" src={Css3} alt="CSS3"/>
                                 <img className="skills-image" src={Bootstrap} alt="Bootstrap"/>
+                                <img className="skills-image" src={Js} alt="JavaScript" />
+
                             <div className='project-btn'>
                                 <button type="button" class="btn btn-primary project-btn"><a href="https://github.com/brettfleming/Cocktails-Cuisine-Craft-Beers" target="_blank">GitHub</a></button>
                                 <button type="button" class="btn btn-success project-btn"><a href="https://brettfleming.github.io/Cocktails-Cuisine-Craft-Beers/" target="_blank">Demo</a></button>
