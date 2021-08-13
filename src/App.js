dfed973aa3c7feddba5dacd0b9fab3b3a874bd8d
Brett Fleming <brettfleming015@gmail.com>
2021-08-13 09:35:59 -0500

    small things

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Projects from "./pages/Projects";
 
+console.log(process.env)
+const routePrefix = (process.env.NODE_ENV === "production") ? "/react-portfolio/": "/"
+
 function App() {
   return (
     <Router>
       <div>
         <Nav />
         <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route exact path='/projects' component={Projects}/>
-          <Route path='/about' component={About}/>
-          <Route exact path='/contact' component={Contact}/>
+          <Route exact path={`${routePrefix}`} component={Home}/>
+          <Route exact path={`${routePrefix}projects`} component={Projects}/>
+          <Route path={`${routePrefix}about`} component={About}/>
+          <Route exact path={`${routePrefix}contact`} component={Contact}/>
         </Switch>
       </div>
     </Router>
